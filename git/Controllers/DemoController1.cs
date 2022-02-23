be1bf166aafd3c02b2bf27855c8c395c2cd46cb2
SagarMutkiri <93123228+SagarMutkiri@users.noreply.github.com>
2022-02-23 16:01:06 +0530

    sagar- v-1.2 Added Demo Controller

---
+++ git/Controllers/DemoController1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace git.Controllers
+{
+    public class DemoController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
